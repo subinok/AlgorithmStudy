c7494c7e6af3bffae11c9bcf3072ad0d6509ca1c
SubinOk <duddldjssl@naver.com>
2022-08-08 11:26:26 +0900

    제목 추가

--- CodingMasters_AIVLE/3275_escapestr.py
+++ CodingMasters_AIVLE/3275_escapestr.py
@@ -1,1 +1,10 @@
+# 3275. real escape string
 # 주어진 문장의 ', ", \를 \', \", \\로 바꾼 문자열을 출력
+
+text = r'\\' # input()
+
+text = text.replace('\\', '\\\\')
+text = text.replace('\'', '\\\'')
+text = text.replace('\"', '\\\"')
+
+print(text)
--- CodingMasters_AIVLE/3279_ipAddr.py
+++ CodingMasters_AIVLE/3279_ipAddr.py
@@ -1,3 +1,4 @@
+# 3279. IP주소
 # IP가 주어졌을 때, 해당 IP가 IPv4 형식인지 IPv6 형식인지 출력
 
 addr = input()
